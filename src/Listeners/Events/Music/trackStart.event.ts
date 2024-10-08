d8cd994c2be92e71abb8bf8c8f69cc6a0a337732
Abhay <130862844+areyouabhay@users.noreply.github.com>
2024-10-08 12:01:42 +0530

    Update trackStart.event.ts

--- src/Listeners/Events/Music/trackStart.event.ts
+++ src/Listeners/Events/Music/trackStart.event.ts
@@ -46,56 +46,6 @@ export default class TrackStart extends Event {
       },
     });
 
-    const x = track.info.artworkUrl; // Image Of Music
-
-    const canvas = Canvas.createCanvas(1000, 360);
-    const ctx = canvas.getContext("2d");
-    const TrackImage = await Canvas.loadImage(x);
-    const TrackTitle = track.info.title
-      .toString()
-      .split(" ")
-      .slice(0, 5)
-      .join(` `);
-
-    // ctx.drawImage(TrackImage, 0, 0, 1200, 560);
-    // StackBlur.canvasRGBA(canvas, 0, 0, canvas.width, canvas.height, 30);
-    // // Track Title
-    // ctx.font = "bold 35px sans-serif";
-    // ctx.fillStyle = "#FFFFFF";
-    // ctx.textAlign = "left";
-    // ctx.fillText(TrackTitle, 40, 140.7);
-    // // Track Author
-    // ctx.font = "30px sans-serif";
-    // ctx.fillStyle = "#FFFFFF";
-    // ctx.textAlign = "left";
-    // ctx.fillText(track.info.author, 40, 180);
-    // // Track Thumbnail
-    // if (track.info.sourceName === "youtube") {
-    //   ctx.drawImage(TrackImage, 716.2, 115.2, 260.8, 150.5);
-    // } else {
-    //   ctx.drawImage(TrackImage, 715.3, 55.8, 240, 240);
-    // }
-
-    // const PlayerImage = new AttachmentBuilder(await canvas.toBuffer(), {
-    //   name: "nirvanamusic.png",
-    // });
-    const colors = await getColors(x.replace("webp", "png"));
-    const embedcolor = colors[0].hex();
-
-    const PlayerEmbed = this.client
-      .embed()
-      .setColor(embedcolor)
-      .setTitle("Now Playing")
-      .setThumbnail(track.info.artworkUrl)
-      .setDescription(
-        `[${track.info.title}](${this.client.config.url.support}) \n-# ${track.info.author}`
-      )
-      .setFooter({
-        text: `Chosen by @${track.info.requester.username}`,
-        iconURL: track.info.requester.displayAvatarURL(),
-      })
-      .setTimestamp();
-
     if (
       guild.members.me
         .permissionsIn(channel)
